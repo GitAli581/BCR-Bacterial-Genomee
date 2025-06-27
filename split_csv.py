d4acc8432808b097b1c84c332a04ed9b6f36e501
GitAli581 <alithehack333@gmail.com>
2025-06-26 22:42:08 -0400

    Update split_csv.py

--- split_csv.py
+++ split_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 input_file = "input.csv"
-rows_per_chunk = 100000  # Adjust this depending on your system and file size
+rows_per_chunk = 100000  # Doesnt rlly matter just choose based on what you want
 
 for i, chunk in enumerate(pd.read_csv(input_file, chunksize=rows_per_chunk)):
     chunk.to_csv(f"input_part_{i+1}.csv", index=False)
